4648afb3b8af12becea15fb3fcb58f02021581d7
DngBack <bachduong7103@gmail.com>
2024-01-02 07:17:42 +0700

    Add code

--- Section1/1_hello.go
+++ Section1/1_hello.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// bắt buộc phải có hàm main 
 func main() {
 	fmt.Println("Hello, mọi người.")
 }
---
+++ Section1/5_var.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// Khai báo gần giống với java với thứ tự vàl var [tên biến] [kiểu dữ liệu]
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
